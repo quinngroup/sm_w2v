2877712fac69bc9c079be1f6d572d11f337d75ca
patrickbreen <pbreen@uga.edu>
2016-03-27 11:56:32 -0400

    move timeline download to __main__

--- sm_w2v/__main__.py
+++ sm_w2v/__main__.py
@@ -1,9 +1,12 @@
 import json
 import sys
 import datetime
+import time
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream
+from tweepy.api import API
+from tweepy.parsers import JSONParser
 
 from sm_w2v.utils import clean_text, clean_tweet, relevant_twt
 from sm_w2v.tokens import CONSUMER_KEY, CONSUMER_SECRET, TOKEN_KEY, TOKEN_SECRET
@@ -32,6 +35,8 @@ track = ['hiv', 'aids', 'pre-exposure', 'prep', 'prophylaxis',
 'Shiga', 'Shigellosis', 'Rickettsiosis', 'Spotted Fever',
 'Syphilis', 'Varicella', 'chicken pox', 'West Nile Virus']
 
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
 
 class StdOutListener(StreamListener):
     # write streaming data to data.json
@@ -54,8 +59,6 @@ def run_download():
         try:
             # open file to write, and authenticate
             l = StdOutListener()
-            auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-            auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
             stream = Stream(auth, l, retry_count=10)
 
             # Use the POST statuses/filter endpoint
@@ -84,6 +87,33 @@ def run_clean():
                 f_out.write(json.dumps(cln_twt) + '\n')
     print("done cleaning.")
 
+def run_user_timeline_download():
+    print('downloading user-timelines...')
+    api = API(auth, parser=JSONParser())
+    user_str_ids = []
+    with open('data/top_users_to_PrEP.txt') as f_in:
+        for line_no, line in enumerate(f_in):
+            if line_no == 1000:
+                break
+            user_str_ids.append(line)
+
+    users = []
+    pages = list(range(0, 150))
+    with open('data/user_timeline_tweets.json', 'w') as f_out:
+        for user_id in user_str_ids:
+            try:
+                time.sleep(60 * 16)
+                for page in pages:
+                    for twt in api.user_timeline(user_id, count=20, page=page):
+                        f_out.write(json.dumps(twt) + '\n')
+                users.append(user_id)
+            except:
+                pass
+
+    print('done with user-timelines...')
+    print(users)
+    print(len(user_str_ids))
+
 if __name__ == '__main__':
     run = sys.argv[1]
 
@@ -91,5 +121,7 @@ if __name__ == '__main__':
         run_download()
     elif run == "clean":
         run_clean()
+    elif run == "timeline":
+        run_user_timeline_download()
     else:
         print("\n\n Sorry, that wasn't a valid choice. The choices are (download, clean, train, wdfrq)\n")
